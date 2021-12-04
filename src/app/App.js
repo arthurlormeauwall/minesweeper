68a91b86ac91fbb21277b4531d446f41fac2cd9e
Arthur Lormeau Wall <arthurlormeau@gmail.com>
2021-12-04 13:12:27 +0100

    first commit setting up project

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import '../scss/custom.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world! </h1>
+      <button type="button" className="btn btn-success">Success</button>
+    </div>
+  );
+}
+
+export default App;
